e484dc7d88792ced00d38eb1e6a93d2a1e1dc5ee
saharshleo <saharshjain2000@gmail.com>
2019-12-12 16:15:11 +0530

    separated receiving and transmitting part with decreased baud rate

--- 433MHz/rfrx_test/main/rfrx_test.c
+++ 433MHz/rfrx_test/main/rfrx_test.c
@@ -15,20 +15,20 @@
 
 static const int RX_BUF_SIZE = 1024;
 #define RXD_PIN (GPIO_NUM_5)
-#define TXD_PIN (GPIO_NUM_4)
+// #define TXD_PIN (GPIO_NUM_4)
 
 
 //UART configurations
 void init(void) {
     const uart_config_t uart_config = {
-        .baud_rate = 9600,
+        .baud_rate = 2400,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
     };
     uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_set_pin(UART_NUM_1, NULL, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
     // We won't use a buffer for sending data.
     uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
 }
--- 433MHz/rftx_test/main/rftx_test.c
+++ 433MHz/rftx_test/main/rftx_test.c
@@ -12,23 +12,23 @@
 // #include "esp_spi_flash.h"
 
 
-static const int RX_BUF_SIZE = 1024;
+// static const int RX_BUF_SIZE = 1024;
 #define TXD_PIN (GPIO_NUM_4)
-#define RXD_PIN (GPIO_NUM_5)
+// #define RXD_PIN (GPIO_NUM_5)
 
 
 void init(void) {
     const uart_config_t uart_config = {
-        .baud_rate = 9600,
+        .baud_rate = 2400,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
     };
     uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_set_pin(UART_NUM_1, TXD_PIN, NULL, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
     // We won't use a buffer for sending data.
-    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_driver_install(UART_NUM_1, 2048, 0, 0, NULL, 0);
 }
 
 int sendData(const char* logName, const char* data)
@@ -44,7 +44,7 @@ static void tx_task(void *arg)
     static const char *TX_TASK_TAG = "TX_TASK";
     esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
     while (1) {
-        sendData(TX_TASK_TAG, "Hello world");
+        sendData(TX_TASK_TAG, "###Hello world###");
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
